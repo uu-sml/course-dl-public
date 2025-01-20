436f83fb169f4f75771b6e2ac0645e13751efd6b
Niklas Wahlstrom <niklas.wahlstrom@it.uu.se>
2025-01-20 11:20:11 +0100

    add assignment template file

---
+++ HA1/load_mnist.py
@@ -0,0 +1,46 @@
+import numpy as np
+import imageio
+import glob
+
+def load_mnist():
+    # Loads the MNIST dataset from png images
+    #
+    # Return
+    # X_train - Training input 
+    # Y_train - Training output (one-hot encoded)
+    # X_test - Test input
+    # Y_test - Test output (one-hot encoded)
+    #
+    # Each of them uses rows as data point dimension.
+ 
+    NUM_LABELS = 10        
+    # create list of image objects
+    test_images = []
+    test_labels = []    
+    
+    for label in range(NUM_LABELS):
+        for image_path in glob.glob("MNIST/Test/" + str(label) + "/*.png"):
+            image = imageio.imread(image_path)
+            test_images.append(image)
+            letter = [0 for _ in range(0,NUM_LABELS)]    
+            letter[label] = 1
+            test_labels.append(letter)  
+            
+    # create list of image objects
+    train_images = []
+    train_labels = []    
+    
+    for label in range(NUM_LABELS):
+        for image_path in glob.glob("MNIST/Train/" + str(label) + "/*.png"):
+            image = imageio.imread(image_path)
+            train_images.append(image)
+            letter = [0 for _ in range(0,NUM_LABELS)]    
+            letter[label] = 1
+            train_labels.append(letter)                  
+            
+    X_train= np.array(train_images).reshape(-1,784)/255.0
+    Y_train= np.array(train_labels)
+    X_test= np.array(test_images).reshape(-1,784)/255.0
+    Y_test= np.array(test_labels)
+    
+    return X_train, Y_train, X_test, Y_test
